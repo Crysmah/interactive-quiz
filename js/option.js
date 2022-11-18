c260ca00dff406ed963db31ceefd881abb4bb2f1
simonkaley <63480942+simonkaley@users.noreply.github.com>
2022-11-18 02:05:44 -0800

    Add files via upload

--- js/option.js
+++ js/option.js
@@ -1,77 +1,84 @@
+//question number; increase if more questions are added
+const qNum ='19';
+
 // Stores all questions and answers in quizQA (Questions & Answers).
 const quizQA = [
     {
-        question: "You have an eye for spotting errors in a project that others have missed.",
+        question: "You have an eye for spotting errors in a project that others have missed. \n(Question 1/"+qNum+")",
         answer: "QA",
     },
     {
-        question: "People often come to you to confirm details about what the project should be.",
+        question: "People often come to you to confirm details about what the project should be. \n(Question 2/"+qNum+")",
         answer: "ProductOwner",
         
     },
     {
-        question: "You are not afraid to voice your opnion and concerns.",
+        question: "You are not afraid to voice your opinion and concerns. \n(Question 3/"+qNum+")",
         answer: "QA",
     },
     {
-        question: "You offer creative new perspectives on a project.",
+        question: "You offer creative new perspectives on a project. \n(Question 4/"+qNum+")",
         answer: "QA",
     },
     {
-        question:  "You are detailed oriented.",
+        question:  "You are detailed oriented. \n(Question 5/"+qNum+")",
         answer: "QA",
     },
     {
-        question: "People often come to you to confirm that they are working on the correct part of the project.",
+        question: "People often come to you to confirm that they are working on the correct part of the project. \n(Question 6/"+qNum+")",
         answer: "ProductOwner",
     },
     {
-        question: "Your friends and family would consider you an extrovert.",
+        question: "Your friends and family would consider you an extrovert. \n(Question 7/"+qNum+")",
         answer: "ProductOwner",
     },
     {
-        question: "You often reach out to absent members in a group to let them know that their absence is noticed.",
+        question: "You often reach out to absent members in a group to let them know that their absence is noticed. \n(Question 8/"+qNum+")",
         answer: "ScrumMaster",
     },
     {
-        question: "People often rely on you to ensure that the progress of the group is going smoothly.",
+        question: "People often rely on you to ensure that the progress of the group is going smoothly. \n(Question 9/"+qNum+")",
         answer: "ScrumMaster",
     },
     {
-        question: "You like to look at the big picture.",
+        question: "You like to look at the big picture. \n(Question 10/"+qNum+")",
         answer: "ScrumMaster",
     },
     {
-        question: "You are well organized.",
+        question: "You are well organized. \n(Question 11/"+qNum+")",
         answer: "ScrumMaster",
     },
     {
-        question: "You often mentor and teach others new skills.",
+        question: "You often mentor and teach others new skills. \n(Question 12/"+qNum+")",
         answer: "TechLead",
     },
     {
-        question: "People often come to you if they are having difficulty with their part of the project.",
+        question: "People often come to you if they are having difficulty with their part of the project. \n(Question 13/"+qNum+")",
         answer: "TechLead",
     },
     {
-        question: "You divide up tasks amoung your team.",
+        question: "You divide up tasks amoung your team. \n(Question 14/"+qNum+")",
         answer: "TechLead",
     },
     {
-        question: "People often confide in you when they need help.",
+        question: "People often confide in you when they need help. \n(Question 15/"+qNum+")",
         answer: "TechLead",
     },
     {
-        question: "You are often the most hands-on person in a project.",
+        question: "You are often the most hands-on person in a project. \n(Question 16/"+qNum+")",
         answer: "TechChaser",
     },
     {
-        question: "You enjoy being creative and taking on challenges.",
+        question: "You enjoy being creative and taking on challenges. \n(Question 17/"+qNum+")",
         answer: "TechChaser",
     },
     {
-        question: "You are often the first to find a solution while working on a project.",
+        question: "You are often the first to find a solution while working on a project. \n(Question 18/"+qNum+")",
         answer: "TechChaser",
+    },
+    {
+        question: "Which of these title(s) do you resonate with the most? \n(Question 19/"+qNum+")",
+        answer: "tiebreaker",
     },
 ];
 
@@ -83,6 +90,14 @@ const quizChoices =
     c:  "Neutral",
     d:  "Disagree",
     e:  "Strongly Disagree",
+};
+const tieBreakerChoices =
+{
+    a:  "Keeper of the Vision",
+    b:  "The Hall Monitor",
+    c:  "The Doomsayer",
+    d:  "First among equals",
+    e:  "The Inventor",
 };
 const quiz = document.getElementById('quiz')
 const answers = document.querySelectorAll('.answer')
@@ -105,11 +120,79 @@ function loadQuiz(){
     const currentQuestion = quizQA[currentQuizQuestion]
     const currentQuizData = quizChoices
     questions.innerText = currentQuestion.question
+    //for testing to display points during test
+    //+"("+points[0]+","+points[1]+","+points[2]+","+points[3]+","+points[4]+")"
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
     e_text.innerText = currentQuizData.e
+}
+function loadTieBreaker(){
+    deselectAnwsers() // Deselects answer for next question. Prevents accidently clicking the submit button twice.
+    const currentQuestion = quizQA[currentQuizQuestion]
+    const currentQuizData = tieBreakerChoices
+    questions.innerText = currentQuestion.question
+
+    var tieQuestions = Array(0, 0, 0, 0, 0);
+
+
+    for(i = 1; i < 5; i++) {// Single Highest Role
+        if(points[i] > points[highestIndex]){ // Compares roles to see which is the highest. 
+            highestIndex = i;
+         }
+    }
+    for(i=0; i < 5; i++){ // Deals with multiple roles being the same value
+        if(points[i] == points[highestIndex]){
+            tieQuestions[i]=1
+        }
+    }
+
+    if (tieQuestions[0]==1)
+    {
+        a_text.innerText = currentQuizData.a
+    }
+    else
+    {
+        document.getElementById("a").hidden = true
+        a_text.innerText = ""
+    }
+    if (tieQuestions[1]==1)
+    {
+        b_text.innerText = currentQuizData.b
+    }
+    else
+    {
+        document.getElementById("b").hidden = true
+        b_text.innerText = ""
+    }
+    if (tieQuestions[2]==1)
+    {
+        c_text.innerText = currentQuizData.c
+    }
+    else
+    {
+        document.getElementById("c").hidden = true
+        c_text.innerText = ""
+    }
+    if (tieQuestions[3]==1)
+    {
+        d_text.innerText = currentQuizData.d
+    }
+    else
+    {
+        document.getElementById("d").hidden = true
+        d_text.innerText = ""
+    }
+    if (tieQuestions[4]==1)
+    {
+        e_text.innerText = currentQuizData.e
+    }
+    else
+    {
+        document.getElementById("e").hidden = true
+        e_text.innerText = ""
+    }
 }
 function deselectAnwsers(){
   answers.forEach(item => item.checked = false)
@@ -140,26 +223,106 @@ submitBtn.addEventListener('click', () =>{ // Looks for user submitting answer.
             points[3] += parseInt(answer)
         else if(quizQA[currentQuizQuestion].answer === "TechChaser")
             points[4] += parseInt(answer)
+            
+        else if(quizQA[currentQuizQuestion].answer === "tiebreaker")
+        {
+            //break tie
+            
+            if(parseInt(answer) === 5)
+            {
+                points[0] += 1
+            }
+            if(parseInt(answer) === 4)
+            {
+                points[1] += 1
+            }
+            if(parseInt(answer) === 3)
+            {
+                points[2] += 1
+            }
+            if(parseInt(answer) === 2)
+            {
+                points[3] += 1
+            }
+            if(parseInt(answer) === 1)
+            {
+                points[4] += 1
+            }
+            
+        }
+        
        currentQuizQuestion++
     
-    if(currentQuizQuestion < quizQA.length){
+    if(currentQuizQuestion < quizQA.length-1){
         loadQuiz()
     } 
+    else if(currentQuizQuestion === quizQA.length-1){
+        loadTieBreaker()
+    }
     else{
         for(i = 1; i < 5; i++) {// Single Highest Role
             if(points[i] > points[highestIndex]){ // Compares roles to see which is the highest. 
                 highestIndex = i;
              }
         }
-        quiz.innerHTML= "Result: Your best suited role(s) is " + roles[highestIndex] 
-        for(i=0; i < 5; i++){ // Deals with multiple roles being the same value
+        quiz.innerHTML= "Result: Your best suited role is " + roles[highestIndex] 
+        for(i=0; i < 5; i++){ // Deals with multiple roles being the same value; appended: this shouldn't be a problem anymore with the tiebreaker 
             if(points[i] == points[highestIndex] && highestIndex !=i){
-       
+
                 quiz.innerHTML+= ", " + roles[i] 
             }
         }
+        
+        var finalRole=roles[highestIndex] 
+
+        if (finalRole==="Product Owner") 
+        {
+            var img = document.createElement("img");
+            img.src = "js/ProductOwner.jpg";
+            quiz.appendChild(img);
+
+            quiz.innerHTML += "<br/>You are the keeper of the vision! You are the authority on the product details and know what shots to take!"
+
+           
+
+        }
+        else if (finalRole==="Scrum Master") 
+        {
+            var img = document.createElement("img");
+            img.src = "js/ScrumMaster.jpg";
+            quiz.appendChild(img);
+
+            quiz.innerHTML += "<br/>You are the Hall Monitor! You help the project stay on track and help call the shots!"
+        }
+        else if (finalRole==="QA") 
+        {
+            var img = document.createElement("img");
+            img.src = "js/QA.jpg";
+            quiz.appendChild(img);
+
+            quiz.innerHTML += "<br/>You are the Doomsayer! You have a keen eye for details and help spot bugs within a project!"
+        }
+        else if (finalRole==="Tech Lead") 
+        {
+            var img = document.createElement("img");
+            img.src = "js/TechLead.jpg";
+            quiz.appendChild(img);
+
+            quiz.innerHTML += "<br/>You are the Tech Master! Your tech knowledge is incredibly valuable to not only the project but also to others who need mentoring!"
+        }
+        else if (finalRole==="Tech Chaser") 
+        {
+            var img = document.createElement("img");
+            img.src = "js/TechChaser.jpg";
+            quiz.appendChild(img);
+
+            quiz.innerHTML += "<br/>You are the Inventor! You are creative and always looking for knowledge!"
+        }
+
+        /*
         quiz.innerHTML += "<br/><br/>Summary of Role(s):"
        
+        
          //product owner
         quiz.innerHTML += "<br/>Product Owner<br/> You are the keeper of the Vision! You are the authority and know what shots to take!"
         //Scrummaster
@@ -172,9 +335,16 @@ submitBtn.addEventListener('click', () =>{ // Looks for user submitting answer.
          quiz.innerHTML += "<br/><br/>Quality Assurance<br/>You are the Doomsayer! You have a keen eye for details and help spot bugs within a project!"
          quiz.innerHTML += "<br/><br/> Click our about page to learn more about your role(s)!</font>"
 
-     quiz.innerHTML+="<br><br>Email: scrumassignerfeedback@gmail.com if you wish to leave us<br>some feedback about our website!"
+         */
+     quiz.innerHTML+="<br><br>Feedback Email: scrumassignerfeedback@gmail.com "
      let btn = document.createElement("button");
-btn.innerHTML = "Share";
+btn.innerHTML = "Copy result";
+//move to center
+btn.style.marginLeft = "460px";
+btn.style.marginTop = "-110px";
+
+
+
 btn.onclick = function () {
     const textRange = document.createRange();
     var range = document.getElementById('quiz');
@@ -194,6 +364,7 @@ btn.onclick = function () {
 };
 document.body.appendChild(btn);
 
+quiz.style.height = "700px";
     
 }
 }
